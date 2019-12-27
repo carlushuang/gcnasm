be2239eb1f90f3f2439a4b3565d876faa3b28b13
carlushuang <carlushuang@hotmail.com>
2019-12-27 15:49:07 +0800

    update fix

--- bandwidth_c/memcpy_kernel.cu
+++ bandwidth_c/memcpy_kernel.cu
@@ -1,53 +1,66 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
+#include <assert.h>
+#include <iostream>
+#include <stdlib.h>
 
 extern "C" __global__
-void memcpy_kernel(unsigned char* __restrict__ output,  unsigned char* __restrict__ input){
+void memcpy_kernel(unsigned char* __restrict__ output, const unsigned char* __restrict__ input){
     output += (blockIdx.x<<13)|(threadIdx.x<<2); 
-    *((float*)&output[0])       = *((float*)&input[0]);
-    *((float*)&output[0x400])   = *((float*)&input[0x400]);
-    *((float*)&output[0x800])   = *((float*)&input[0x800]);
-    *((float*)&output[0xc00])   = *((float*)&input[0xc00]);
-    *((float*)&output[0x1000])  = *((float*)&input[0x1000]);
-    *((float*)&output[0x1400])  = *((float*)&input[0x1400]);
-    *((float*)&output[0x1800])  = *((float*)&input[0x1800]);
-    *((float*)&output[0x1c00])  = *((float*)&input[0x1c00]);
+    *((float* __restrict__ )&output[0])       = *((float* __restrict__ )&input[0]);
+    *((float* __restrict__ )&output[0x400])   = *((float* __restrict__ )&input[0x400]);
+    *((float* __restrict__ )&output[0x800])   = *((float* __restrict__ )&input[0x800]);
+    *((float* __restrict__ )&output[0xc00])   = *((float* __restrict__ )&input[0xc00]);
+    *((float* __restrict__ )&output[0x1000])  = *((float* __restrict__ )&input[0x1000]);
+    *((float* __restrict__ )&output[0x1400])  = *((float* __restrict__ )&input[0x1400]);
+    *((float* __restrict__ )&output[0x1800])  = *((float* __restrict__ )&input[0x1800]);
+    *((float* __restrict__ )&output[0x1c00])  = *((float* __restrict__ )&input[0x1c00]);
 }
+
+#define CALL(cmd) \
+do {\
+    cudaError_t cuda_error  = cmd;\
+    if (cuda_error != cudaSuccess) { \
+        std::cout<<"'"<<cudaGetErrorString(cuda_error)<<"'("<<cuda_error<<")"<<" at "<<__FILE__<<":"<<__LINE__<<std::endl;\
+        exit(EXIT_FAILURE);\
+    }\
+} while(0)
+
 #define WARMUP 2
 #define LOOP 10
 
 int main() {
 	cudaSetDevice(0);
-    float *A, *B, *output;
+    unsigned char *A, *B;
     const int total_float =64*3*224*224;
 	static float h_A[total_float], h_B[total_float];
 	for (int i = 0; i < total_float; ++i)
 		h_A[i] = i % 71;
 
-	cudaMalloc(&A, total_float * sizeof(float));
-	cudaMalloc(&B, total_float * sizeof(float));
-	cudaMemcpy(A, h_A, sizeof(h_A), cudaMemcpyHostToDevice);
-	//cudaMemcpy(B, h_B, sizeof(h_B), cudaMemcpyHostToDevice);
+    CALL(cudaMalloc(&A, total_float * sizeof(float)));
+    CALL(cudaMalloc(&B, total_float * sizeof(float)));
+    CALL(cudaMemcpy(A, h_A, total_float * sizeof(float), cudaMemcpyHostToDevice));
+
     int bx = 256;
     int gx = (total_float+255)>>8;
+    assert(total_float/bx);
 
     cudaEvent_t start_ev, stop_ev;
-    cudaEventCreate(&start_ev);
-    cudaEventCreate(&stop_ev);
+    CALL(cudaEventCreate(&start_ev));
+    CALL(cudaEventCreate(&stop_ev));
 
     for(int i=0;i<WARMUP;i++)
-        memcpy_kernel<<<gx, bx, 0, 0>>>(B, A);
+        memcpy_kernel<<<gx, bx>>>(B, A);
 
-    cudaDeviceSynchronize();
-    cudaEventRecord( start_ev, NULL);
+    CALL(cudaDeviceSynchronize());
+    CALL(cudaEventRecord( start_ev, 0));
     for(int i=0;i<LOOP;i++)
-        memcpy_kernel<<<gx, bx, 0, 0>>>(B, A);
-    cudaEventRecord( stop_ev, NULL );
+        memcpy_kernel<<<gx, bx>>>(B, A);
+    CALL(cudaEventRecord( stop_ev, 0 ));
 
     float ms;
-    cudaEventElapsedTime(&ms,start_ev, stop_ev);
+    CALL(cudaEventElapsedTime(&ms,start_ev, stop_ev));
     ms/=LOOP;
 
-    printf("total %dB, gflops:%f\n", (total_float*sizeof(float)*2)/((double)ms/1000)/1000000000.0 );
-
+    printf("total %dB, gflops:%f\n", total_float, ((double)total_float*sizeof(float)*2)/((double)ms/1000)/1000000000.0 );
 }
