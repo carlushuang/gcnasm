ab6f0b4a17aa2b164f48cb248df0b01424776c64
carlushuang <carlushuang@hotmail.com>
2020-05-08 10:43:48 +0000

    add memcopy example

---
+++ memcpy_example/main.cpp
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <hip/hip_runtime.h>
+#include <random>
+#include <iostream>
+
+#define HIP_CALL(call) do{  \
+    hipError_t err = call;  \
+    if(err != hipSuccess){  \
+        printf("[hiperror](%d) fail to call %s",(int)err,#call);    \
+        exit(0);            \
+    }                      \
+} while(0)
+
+#define HSACO "memcpy_kernel.hsaco"
+#define HSA_KERNEL "memcpy_kernel"
+
+
+#define PER_PIXEL_CHECK
+#define ASSERT_ON_FAIL
+
+#ifndef ABS
+#define ABS(x) ((x)>0?(x):-1*(x))
+#endif
+
+template<typename T>
+void rand_vec(T * seq, size_t len){
+    static std::random_device rd;   // seed
+    static std::mt19937 mt(rd());
+    static std::uniform_real_distribution<T> dist(-10.0, 10.0);
+
+    for(size_t i=0;i<len;i++) seq[i] =  dist(mt);
+}
+
+static inline bool valid_vector( const float* ref, const float* pred, int n, double nrms = 1e-6 )
+{
+    double s0=0.0;
+    double s1=0.0;
+#ifdef PER_PIXEL_CHECK
+    int pp_err = 0;
+#endif
+    for( int i=0; i<n; ++i ){
+        double ri=(double)ref[i];
+        double pi=(double)pred[i];
+        double d=ri-pi;
+        double dd=d*d;
+        double rr=2.0*ri*ri;
+        s0+=dd;
+        s1+=rr;
+#ifdef PER_PIXEL_CHECK
+        double delta = ABS(ri-pi)/ri;
+        if(delta>3e-5){
+#ifdef ASSERT_ON_FAIL
+            if(pp_err<100)
+                printf("diff at %4d, ref:%lf, pred:%lf(0x%08x), d:%lf\n",i,ri,pi,((uint32_t*)pred)[i],delta);
+#endif
+            pp_err++;
+        }
+#endif
+    }
+    //printf("nrms:%lf, s0:%lf, s1:%lf\n",sqrt(s0/s1),s0,s1);
+    return (sqrt(s0/s1)<nrms)
+#ifdef PER_PIXEL_CHECK
+        && (pp_err==0)
+#endif
+    ;
+}
+
+int main(int argc, char ** argv){
+    hipModule_t module;
+    hipFunction_t kernel_func;
+    hipEvent_t evt_00, evt_11;
+    HIP_CALL(hipSetDevice(0));
+
+    HIP_CALL(hipModuleLoad(&module, HSACO));
+    HIP_CALL(hipModuleGetFunction(&kernel_func, module, HSA_KERNEL));
+
+    int num_cu;
+    {
+        hipDeviceProp_t dev_prop;
+        hipDevice_t dev;
+        HIP_CALL(hipGetDevice( &dev ));
+        HIP_CALL(hipGetDeviceProperties( &dev_prop, dev ));
+        num_cu = dev_prop.multiProcessorCount;
+    }
+
+    int total_loop=4;
+    int warm_ups = 2;
+    int i;
+    int bdx = 256;
+    int gdx = num_cu;
+    float * host_in, * host_out, *dev_in, *dev_out;
+    int loops_per_block = 512;
+    int total_floats = num_cu * 256 * 16 * loops_per_block;
+
+    host_in   = new float[total_floats];
+    host_out  = new float[total_floats];
+    HIP_CALL(hipMalloc(&dev_in,  sizeof(float) * total_floats));
+    HIP_CALL(hipMalloc(&dev_out, sizeof(float) * total_floats));
+
+    rand_vec(host_in, total_floats);
+
+    HIP_CALL(hipMemcpy(dev_in, host_in, sizeof(float)*total_floats, hipMemcpyHostToDevice));
+
+    printf("memcpy, input:%p, output:%p, floats:%d\n",dev_in,dev_out, total_floats);
+
+    struct __attribute__((packed)){
+        float * input;
+        float * output;
+        int     loops_per_block;
+        int     __pack0;
+    } args;
+    size_t arg_size = sizeof(args);
+    args.input = dev_in;
+    args.output = dev_out;
+    args.loops_per_block = loops_per_block;
+
+    void* config[] = {HIP_LAUNCH_PARAM_BUFFER_POINTER, &args, HIP_LAUNCH_PARAM_BUFFER_SIZE,
+                    &arg_size, HIP_LAUNCH_PARAM_END};
+
+    for(i=0;i<warm_ups;i++)
+        HIP_CALL(hipModuleLaunchKernel(kernel_func, gdx,1,1, bdx,1,1,  0, 0, NULL, (void**)&config ));
+
+    hipEventCreate(&evt_00);
+    hipEventCreate(&evt_11);
+
+    hipCtxSynchronize();
+    hipEventRecord(evt_00, NULL);
+    for(i=0;i<total_loop;i++)
+        HIP_CALL(hipModuleLaunchKernel(kernel_func, gdx,1,1, bdx,1,1,  0, 0, NULL, (void**)&config ));
+
+    float elapsed_ms;
+    hipEventRecord(evt_11, NULL);
+    hipEventSynchronize(evt_11);
+    hipCtxSynchronize();
+    hipEventElapsedTime(&elapsed_ms, evt_00, evt_11);
+    hipEventDestroy(evt_00);
+    hipEventDestroy(evt_11);
+
+    HIP_CALL(hipMemcpy(host_out, dev_out, sizeof(float)*total_floats, hipMemcpyDeviceToHost));
+
+    bool is_valid = valid_vector(host_in, host_out, total_floats);
+    if(!is_valid){
+        printf("not valid, please check\n");
+    }
+
+    float time_per_loop_ms = elapsed_ms/total_loop;
+    float gbps = total_floats*2*sizeof(float) / time_per_loop_ms / 1000 / 1000;
+    printf("gbps:%f\n",gbps);
+
+    delete [] host_in;
+    delete [] host_out;
+    hipFree(dev_in);
+    hipFree(dev_out);
+}
---
+++ memcpy_example/memcpy_kernel.s
@@ -0,0 +1,135 @@
+.text
+.global memcpy_kernel
+.p2align 8
+.type memcpy_kernel,@function
+memcpy_kernel:
+; This is just an example, not the optimal one
+.set s_dptr,            0
+.set s_karg,            2
+.set s_bx,              4
+
+.set s_ptr_in,          6
+.set s_ptr_out,         8
+.set s_loops_per_block, 10
+.set s_stride_block,    12
+.set s_tmp,             14
+.set s_gdx,             18
+
+.set v_buf,             0
+.set v_offset,          16
+.set v_tmp,             32
+
+    ; http://www.hsafoundation.com/html/Content/Runtime/Topics/02_Core/hsa_kernel_dispatch_packet_t.htm
+    ;s_load_dword s[s_gdx],                  s[s_dptr:s_dptr+1], 12
+    ;s_waitcnt           lgkmcnt(0)
+    ;s_lshr_b32      s[s_gdx],   s[s_gdx],   8
+    s_mov_b32   s[s_gdx], 60
+
+    s_load_dwordx2 s[s_ptr_in:s_ptr_in+1],      s[s_karg:s_karg+1],     0
+    s_load_dwordx2 s[s_ptr_out:s_ptr_out+1],    s[s_karg:s_karg+1],     8
+    s_load_dword s[s_loops_per_block],          s[s_karg:s_karg+1],     16
+
+    s_mul_i32 s[s_tmp+1], s[s_bx], 256*4
+    v_lshlrev_b32 v[v_tmp], 2, v0
+    v_add_u32 v[v_offset+0], s[s_tmp+1], v[v_tmp]
+
+    s_mul_i32 s[s_tmp],  s[s_gdx],  256*4
+    v_add_u32 v[v_offset+1],    s[s_tmp],   v[v_offset+0]
+    v_add_u32 v[v_offset+2],    s[s_tmp],   v[v_offset+1]
+    v_add_u32 v[v_offset+3],    s[s_tmp],   v[v_offset+2]
+    v_add_u32 v[v_offset+4],    s[s_tmp],   v[v_offset+3]
+    v_add_u32 v[v_offset+5],    s[s_tmp],   v[v_offset+4]
+    v_add_u32 v[v_offset+6],    s[s_tmp],   v[v_offset+5]
+    v_add_u32 v[v_offset+7],    s[s_tmp],   v[v_offset+6]
+    v_add_u32 v[v_offset+8],    s[s_tmp],   v[v_offset+7]
+    v_add_u32 v[v_offset+9],    s[s_tmp],   v[v_offset+8]
+    v_add_u32 v[v_offset+10],   s[s_tmp],   v[v_offset+9]
+    v_add_u32 v[v_offset+11],   s[s_tmp],   v[v_offset+10]
+    v_add_u32 v[v_offset+12],   s[s_tmp],   v[v_offset+11]
+    v_add_u32 v[v_offset+13],   s[s_tmp],   v[v_offset+12]
+    v_add_u32 v[v_offset+14],   s[s_tmp],   v[v_offset+13]
+    v_add_u32 v[v_offset+15],   s[s_tmp],   v[v_offset+14]
+    s_lshl_b32 s[s_stride_block],   s[s_tmp],   4   ; unroll 16
+
+    s_waitcnt           lgkmcnt(0)
+
+label_memcopy_start:
+    global_load_dword   v[v_buf+0],  v[v_offset+0:v_offset+1],   s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+1],  v[v_offset+1:v_offset+2],   s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+2],  v[v_offset+2:v_offset+3],   s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+3],  v[v_offset+3:v_offset+4],   s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+4],  v[v_offset+4:v_offset+5],   s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+5],  v[v_offset+5:v_offset+6],   s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+6],  v[v_offset+6:v_offset+7],   s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+7],  v[v_offset+7:v_offset+8],   s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+8],  v[v_offset+8:v_offset+9],   s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+9],  v[v_offset+9:v_offset+10],  s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+10], v[v_offset+10:v_offset+11], s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+11], v[v_offset+11:v_offset+12], s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+12], v[v_offset+12:v_offset+13], s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+13], v[v_offset+13:v_offset+14], s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+14], v[v_offset+14:v_offset+15], s[s_ptr_in:s_ptr_in+1]
+    global_load_dword   v[v_buf+15], v[v_offset+15:v_offset+16], s[s_ptr_in:s_ptr_in+1]
+
+    s_add_u32   s[s_ptr_in],   s[s_stride_block], s[s_ptr_in]
+    s_addc_u32  s[s_ptr_in+1], s[s_ptr_in+1], 0
+
+    s_waitcnt       vmcnt(0)
+
+    global_store_dword  v[v_offset+0:v_offset+1],   v[v_buf+0],   s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+1:v_offset+2],   v[v_buf+1],   s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+2:v_offset+3],   v[v_buf+2],   s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+3:v_offset+4],   v[v_buf+3],   s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+4:v_offset+5],   v[v_buf+4],   s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+5:v_offset+6],   v[v_buf+5],   s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+6:v_offset+7],   v[v_buf+6],   s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+7:v_offset+8],   v[v_buf+7],   s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+8:v_offset+9],   v[v_buf+8],   s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+9:v_offset+10],  v[v_buf+9],   s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+10:v_offset+11], v[v_buf+10],  s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+11:v_offset+12], v[v_buf+11],  s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+12:v_offset+13], v[v_buf+12],  s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+13:v_offset+14], v[v_buf+13],  s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+14:v_offset+15], v[v_buf+14],  s[s_ptr_out:s_ptr_out+1]
+    global_store_dword  v[v_offset+15:v_offset+16], v[v_buf+15],  s[s_ptr_out:s_ptr_out+1]
+
+    s_add_u32   s[s_ptr_out],   s[s_stride_block], s[s_ptr_out]
+    s_addc_u32  s[s_ptr_out+1], s[s_ptr_out+1], 0
+
+    s_sub_u32 s[s_loops_per_block], s[s_loops_per_block], 1
+    s_cmp_eq_u32 s[s_loops_per_block], 0
+    s_waitcnt       vmcnt(0)
+    s_cbranch_scc0  label_memcopy_start
+    s_endpgm
+
+.rodata
+.p2align 6
+.amdhsa_kernel memcpy_kernel
+    .amdhsa_group_segment_fixed_size 0
+    .amdhsa_user_sgpr_dispatch_ptr 1
+    .amdhsa_user_sgpr_kernarg_segment_ptr 1
+    .amdhsa_system_sgpr_workgroup_id_x 1
+    .amdhsa_system_vgpr_workitem_id 0
+    .amdhsa_next_free_vgpr 64
+    .amdhsa_next_free_sgpr 32
+    .amdhsa_ieee_mode 0
+    .amdhsa_dx10_clamp 0
+.end_amdhsa_kernel
+
+.amdgpu_metadata
+---
+amdhsa.version: [ 1, 0 ]
+amdhsa.kernels:
+  - .name: memcpy_kernel
+    .symbol: memcpy_kernel.kd
+    .sgpr_count: 32
+    .vgpr_count: 64
+    .kernarg_segment_align: 8
+    .kernarg_segment_size: 24
+    .group_segment_fixed_size: 0
+    .private_segment_fixed_size: 0
+    .wavefront_size: 64
+    .reqd_workgroup_size : [256, 1, 1]
+    .max_flat_workgroup_size: 256
+...
+.end_amdgpu_metadata
