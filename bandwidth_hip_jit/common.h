9bb10fc4991849aacc879c23439dc177595c355f
carlushuang <carlus.huang@amd.com>
2023-04-14 16:16:38 +0000

    inline emit

--- bandwidth_hip_jit/common.h
+++ bandwidth_hip_jit/common.h
@@ -13,6 +13,8 @@ using fp32x4 = __attribute__((__ext_vector_type__(4))) float;
 #include <stdio.h>
 #include <array>
 #include <unistd.h>
+#include <fstream>
+#include <iostream>
 
 #ifndef PWD
 #define PWD ""
@@ -30,11 +32,13 @@ using fp32x4 = __attribute__((__ext_vector_type__(4))) float;
 #define MODULE_SO "memcpy_kernel.so"
 #endif
 
+
 typedef void (*memtpy_kernel_t)(void* dst, const void* src, uint32_t dwords);
 
 struct memcpy_module_t{
     memcpy_module_t() : handle(nullptr)
     {
+        emit();
         compile();
         load();
     }
@@ -46,7 +50,7 @@ struct memcpy_module_t{
 
     std::string get_src_path() const
     {
-        return std::string(PWD) + std::string("/") + std::string(MODULE_FILE);
+        return std::string(TMP) + std::string("/") + std::string(MODULE_FILE);
     }
 
     std::string get_so_path() const
@@ -57,6 +61,71 @@ struct memcpy_module_t{
     bool file_exists(const std::string path) const
     {
         return access(path.c_str(), 0) == 0;
+    }
+    
+    void emit() {
+        static const std::string kernel_code =
+"#include <hip/hip_runtime.h>\n"
+"#include <stdint.h>\n"
+"#ifdef __NVCC__\n"
+"using fp32 =  float;\n"
+"using fp32x2 = float2;\n"
+"using fp32x4 = float4;\n"
+"#else\n"
+"using fp32 = __attribute__((__ext_vector_type__(1))) float;\n"
+"using fp32x2 = __attribute__((__ext_vector_type__(2))) float;\n"
+"using fp32x4 = __attribute__((__ext_vector_type__(4))) float;\n"
+"#endif\n"
+"#define BLOCK_SIZE 256\n"
+"template<typename T>\n"
+"__global__\n"
+"void memcpy_kernel(T* __restrict__ dst, const T* __restrict__ src, uint32_t n){\n"
+"    int idx = (blockIdx.x * BLOCK_SIZE + threadIdx.x);\n"
+"    if(idx < n)\n"
+"        dst[idx] = src[idx];\n"
+"}\n"
+"\n"
+"template<typename T>\n"
+"struct memcpy_with_type\n"
+"{\n"
+"    static void run(void * B, const void * A, int dwords){\n"
+"        int bx = BLOCK_SIZE;\n"
+"        int pixels = dwords * 4 / sizeof(T);\n"
+"        int gx = (pixels + BLOCK_SIZE - 1) / BLOCK_SIZE;\n"
+"        memcpy_kernel<T><<<gx, bx>>>(reinterpret_cast<T*>(B), reinterpret_cast<const T*>(A), pixels);\n"
+"    }\n"
+"};\n"
+"\n"
+"extern \"C\"\n"
+"void memcpy_fp32(void* dst, const void* src, uint32_t dwords)\n"
+"{\n"
+"    memcpy_with_type<fp32>::run(dst, src, dwords);\n"
+"}\n"
+"\n"
+"extern \"C\"\n"
+"void memcpy_fp32x2(void* dst, const void* src, uint32_t dwords)\n"
+"{\n"
+"    memcpy_with_type<fp32x2>::run(dst, src, dwords);\n"
+"}\n"
+"\n"
+"extern \"C\"\n"
+"void memcpy_fp32x4(void* dst, const void* src, uint32_t dwords)\n"
+"{\n"
+"    memcpy_with_type<fp32x4>::run(dst, src, dwords);\n"
+"}\n";
+
+        if(file_exists(get_src_path())){
+            return ;
+        }
+        std::ofstream out(get_src_path());
+        if(!out) {
+            printf("error while open file %s\n", get_src_path().c_str());
+            exit(0);
+        }
+        out << kernel_code;
+        out.close();
+        printf("emitted kernel => %s\n", get_src_path().c_str());
+        fflush(stdout);
     }
 
     void compile()
@@ -86,6 +155,8 @@ struct memcpy_module_t{
             printf("%s\n", result.c_str());
             exit(0);
         }
+        printf("compile success so => %s\n", get_so_path().c_str());
+        fflush(stdout);
     }
 
     void load()
