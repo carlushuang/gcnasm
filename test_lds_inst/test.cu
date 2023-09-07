1d1d1c3671f9ffbfc5c8a22e147ea231fa49309d
carlushuang <carlus.huang@amd.com>
2023-09-07 15:56:58 +0000

    fix bug

--- test_lds_inst/test.cu
+++ test_lds_inst/test.cu
@@ -26,10 +26,132 @@ struct integral_constant {
 template<index_t v>
 using number = integral_constant<index_t, v>;
 
+template<typename T, T... Ns>
+struct sequence;
+
+template<index_t...Ns>
+using integer_sequence = sequence<index_t, Ns...>;
+
+#if __has_builtin(__type_pack_element) && !defined(__CUDACC__)
+template<index_t N>
+using make_integer_sequence = __make_integer_seq<sequence, index_t, N>; // TODO: check builtin __make_integer_seq
+#else
+namespace impl
+{
+    // we have four instantiations of generate_sequence<>, independent of T or N.
+    // V is the current bit, E is the end marker - if true, this is the last step.
+    template< bool V, bool E >
+    struct generate_sequence;
+
+    // last step: generate final integer sequence
+    template<>
+    struct generate_sequence< false, true >
+    {
+        template< typename T, T M, T N, index_t S, T... Ns >
+        using f = sequence< T, Ns... >;
+    };
+
+    template<>
+    struct generate_sequence< true, true >
+    {
+        template< typename T, T M, T N, index_t S, T... Ns >
+        using f = sequence< T, Ns..., S >;
+    };
+
+    // intermediate step: double existing values, append one more if V is set.
+    template<>
+    struct generate_sequence< false, false >
+    {
+        template< typename T, T M, T N, index_t S, T... Ns >
+        using f = typename generate_sequence< ( N & ( M / 2 ) ) != 0, ( M / 2 ) == 0 >::template f< T, M / 2, N, 2 * S, Ns..., ( Ns + S )... >;
+    };
+
+    template<>
+    struct generate_sequence< true, false >
+    {
+        template< typename T, T M, T N, index_t S, T... Ns >
+        using f = typename generate_sequence< ( N & ( M / 2 ) ) != 0, ( M / 2 ) == 0 >::template f< T, M / 2, N, 2 * S + 1, Ns..., ( Ns + S )..., 2 * S >;
+    };
+
+    // the final sequence per T/N should be memoized, it will probably be used multiple times.
+    // also checks the limit and starts the above generator properly.
+    template< typename T, T N >
+    struct memoize_sequence
+    {
+        static_assert( N < T( 1 << 20 ), "N too large" );
+        using type = typename generate_sequence< false, false >::template f< T, ( N < T( 1 << 1 ) ) ? T( 1 << 1 ) : ( N < T( 1 << 2 ) ) ? T( 1 << 2 ) : ( N < T( 1 << 3 ) ) ? T( 1 << 3 ) : ( N < T( 1 << 4 ) ) ? T( 1 << 4 ) : ( N < T( 1 << 5 ) ) ? T( 1 << 5 ) : ( N < T( 1 << 6 ) ) ? T( 1 << 6 ) : ( N < T( 1 << 7 ) ) ? T( 1 << 7 ) : ( N < T( 1 << 8 ) ) ? T( 1 << 8 ) : ( N < T( 1 << 9 ) ) ? T( 1 << 9 ) : ( N < T( 1 << 10 ) ) ? T( 1 << 10 ) : T( 1 << 20 ), N, 0 >;
+    };
+}  // namespace impl
+template<index_t N >
+using make_integer_sequence = typename impl::memoize_sequence<index_t, N >::type;
+#endif
+
+
 namespace impl {
+#if __has_builtin(__type_pack_element) && !defined(__CUDACC__)
     // TODO: check builtin __type_pack_element
     template<index_t I, typename... Ts>
     using at_index_t = __type_pack_element<I, Ts... >;
+#else
+      namespace impl
+      {
+         template< std::size_t, typename T >
+         struct indexed
+         {
+            using type = T;
+         };
+
+         template< typename, typename... Ts >
+         struct indexer;
+
+         template< std::size_t... Is, typename... Ts >
+         struct indexer< integer_sequence< Is... >, Ts... >
+            : indexed< Is, Ts >...
+         {
+         };
+
+#if( __cplusplus >= 201402L )
+         template< typename... Ts >
+         constexpr impl::indexer< make_integer_sequence< sizeof...(Ts) >, Ts... > index_value{};
+#endif
+
+         template< std::size_t I, typename T >
+         indexed< I, T > select( const indexed< I, T >& );
+
+      }  // namespace impl
+
+#if( __cplusplus >= 201402L )
+
+      template< std::size_t I, typename... Ts >
+      using at_index = decltype( impl::select< I >( impl::index_value< Ts... > ) );
+
+#else
+
+      template< std::size_t I, typename... Ts >
+      using at_index = decltype( impl::select< I >( impl::indexer< make_integer_sequence< Ts... >, Ts... >() ) );
+
+#endif
+
+#ifndef _MSC_VER
+      template< std::size_t I, typename... Ts >
+      using at_index_t = typename at_index< I, Ts... >::type;
+
+#else
+
+      namespace impl
+      {
+         template< typename T >
+         struct get_type
+         {
+            using type = typename T::type;
+         };
+
+      }  // namespace impl
+
+      template< std::size_t I, typename... Ts >
+      using at_index_t = typename impl::get_type< at_index< I, Ts... > >::type;
+#endif
+#endif
 }
 
 template<typename T, T... Ns>
@@ -51,8 +173,6 @@ struct sequence {
     }
 };
 
-template<index_t N>
-using make_integer_sequence = __make_integer_seq<sequence, index_t, N>; // TODO: check builtin __make_integer_seq
 
 template<index_t... Ns>
 using integer_sequence = sequence<index_t, Ns...>;
