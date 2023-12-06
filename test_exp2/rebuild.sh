4c729fde01a0351bed4ddc33b667be306630710c
carlushuang <carlus.huang@amd.com>
2023-12-06 19:07:29 +0800

    add cuda

--- test_exp2/rebuild.sh
+++ test_exp2/rebuild.sh
@@ -18,9 +18,12 @@ rm -rf $BUILD ; mkdir $BUILD ; cd $BUILD
 if [ "x$TARGET" = "xrocm" ] ; then
 echo "===== build rocm"
 /opt/rocm/bin/hipify-perl $TOP/$SRC > $BUILD/$SRC.hip.cc
-/opt/rocm/bin/hipcc $BUILD/$SRC.hip.cc -fPIC -std=c++17 -O3 -Wall --offload-arch=gfx90a -save-temps -o $BUILD/$OUT
+/opt/rocm/bin/hipcc $BUILD/$SRC.hip.cc  -fgpu-flush-denormals-to-zero  -fPIC -std=c++17 -O3 -Wall --offload-arch=gfx90a -save-temps -o $BUILD/$OUT
 fi
 
 if [ "x$TARGET" = "xcuda" ] ; then
 echo "===== build cuda"
+#  --use_fast_math  add or not is the same
+/usr/local/cuda/bin/nvcc -std=c++17 -O3 $TOP/$SRC -o $BUILD/$OUT
+/usr/local/cuda/bin/cuobjdump $BUILD/$OUT -sass -ptx > $BUILD/dump.txt
 fi
