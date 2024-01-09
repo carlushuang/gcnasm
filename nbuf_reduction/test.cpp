45b7f207396354095460ff85a58a8974f43f3c78
carlushuang <carlus.huang@amd.com>
2024-01-09 04:10:15 -0500

    more data

--- nbuf_reduction/test.cpp
+++ nbuf_reduction/test.cpp
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <numeric>
 
-#define USE_INLINE_ASM 1
+#define USE_INLINE_ASM 0
 #define DYNAMIC_BUF 1
 
 using index_t = int;
@@ -123,7 +123,7 @@ template<> struct gld<16>{
     template<typename T>
     __device__ void operator()(T & value, dwordx4_t res/*buffer resource*/, index_t v_offset, index_t s_offset, index_t i_offset/*max 0xFFF*/, index_t /*flag*/ = 0){
         static_assert(sizeof(T) == 16);
-#ifdef USE_INLINE_ASM
+#if USE_INLINE_ASM
         asm volatile("buffer_load_dwordx4 %0, %1, %2, %3 offen offset:%4"
             : "+v"(value.get()) : "v"(v_offset), "s"(res), "s"(s_offset), "n"(i_offset) : "memory");
 #else
@@ -137,7 +137,7 @@ template<> struct gld<8>{
     template<typename T>
     __device__ void operator()(T & value, dwordx4_t res/*buffer resource*/, index_t v_offset, index_t s_offset, index_t i_offset/*max 0xFFF*/, index_t /*flag*/ = 0){
         static_assert(sizeof(T) == 8);
-#ifdef USE_INLINE_ASM
+#if USE_INLINE_ASM
         asm volatile("buffer_load_dwordx2 %0, %1, %2, %3 offen offset:%4"
             : "+v"(value) : "v"(v_offset), "s"(res), "s"(s_offset), "n"(i_offset) : "memory");
 #else
@@ -151,7 +151,7 @@ template<> struct gld<4>{
     template<typename T>
     __device__ void operator()(T & value, dwordx4_t res/*buffer resource*/, index_t v_offset, index_t s_offset, index_t i_offset/*max 0xFFF*/, index_t /*flag*/ = 0){
         static_assert(sizeof(T) == 4);
-#ifdef USE_INLINE_ASM
+#if USE_INLINE_ASM
         asm volatile("buffer_load_dword %0, %1, %2, %3 offen offset:%4"
             : "+v"(value) : "v"(v_offset), "s"(res), "s"(s_offset), "n"(i_offset) : "memory");
 #else
@@ -163,7 +163,7 @@ template<> struct gld<4>{
 
 __device__ void gld_fence(index_t cnt)
 {
-#ifdef USE_INLINE_ASM
+#if USE_INLINE_ASM
     asm volatile("s_waitcnt vmcnt(%0)" : : "n" (cnt) : "memory");
 #else
     (void) cnt;
@@ -173,7 +173,7 @@ __device__ void gld_fence(index_t cnt)
 template<typename T, index_t N>
 __device__ void gld_fence(vector_type<T, N> & /*buf*/, index_t cnt)
 {
-#ifdef USE_INLINE_ASM
+#if USE_INLINE_ASM
     asm volatile("s_waitcnt vmcnt(%0)" : : "n" (cnt) : "memory");
     // constexpr index_t total = sizeof(T) * N / sizeof(float);
     // for(auto i = 0; i < total; i++) {
@@ -187,7 +187,7 @@ __device__ void gld_fence(vector_type<T, N> & /*buf*/, index_t cnt)
 template<typename T, index_t N>
 __device__ void clear_buf(vector_type<T, N> & buf)
 {
-#ifdef USE_INLINE_ASM
+#if USE_INLINE_ASM
     for(auto i = 0; i < N; i++)
         asm volatile("v_mov_b32 %0, 0" : "+v"(buf.at(i)) :  : "memory");
 #else
@@ -201,7 +201,7 @@ __device__ void v_acc(T & x, const W & a, const W & b)
 {
     // TODO: T/W must be vector type
     static_assert(T::size == W::size);
-#ifdef USE_INLINE_ASM
+#if USE_INLINE_ASM
     // TODO: force to fp32
     for(auto i = 0; i < T::size; i++)
         asm volatile("v_fmac_f32 %0, %1, %2" : "+v"(x.at(i)) : "v"(a.at(i)), "v"(b.at(i)):);
@@ -211,9 +211,9 @@ __device__ void v_acc(T & x, const W & a, const W & b)
 #endif
 }
 
+#if 1
 __global__ void
-__launch_bounds__(256, 2)
-reduce(const void* ptr_a,
+reduce_n2(const void* ptr_a,
         const void* ptr_b,
             void* ptr_dst,
             uint32_t rows)
@@ -312,7 +312,165 @@ reduce(const void* ptr_a,
 #endif
     p_dst[col_offset] = acc;
 }
+#endif
 
+// __device__ int amdgcn_if(int v) __asm("llvm.amdgcn.ixxxf");
+
+#if 1
+__global__ void
+reduce_n3(const void* __restrict__ ptr_a,
+            const void* __restrict__ ptr_b,
+            void* __restrict__ ptr_dst,
+            uint32_t rows)
+{
+    if(blockIdx.x > 0)
+        return;
+
+    int col_offset = threadIdx.x;
+
+    using buf_type = vector_type<float, 4>;
+
+    //buf_type acc {.0f};
+    buf_type acc;
+    clear_buf(acc);
+
+    static_buffer<buf_type, 3> g_a;
+    static_buffer<buf_type, 3> g_b;
+    int mod = __builtin_amdgcn_readfirstlane(rows & 2);
+
+    const buf_type * __restrict__ p_a = reinterpret_cast<const buf_type* __restrict__>(ptr_a);
+    const buf_type * __restrict__ p_b = reinterpret_cast<const buf_type* __restrict__>(ptr_b);
+    buf_type * __restrict__ p_dst = reinterpret_cast<buf_type* __restrict__>(ptr_dst);
+
+    int ir = __builtin_amdgcn_readfirstlane(0);
+
+#define GLD_A(i_buf_) gld<sizeof(buf_type)>{}(g_a.get(i_buf_), make_buffer_resource(p_a), col_offset * sizeof(buf_type), ir*256*sizeof(buf_type), 0)
+#define GLD_B(i_buf_) gld<sizeof(buf_type)>{}(g_b.get(i_buf_), make_buffer_resource(p_b), col_offset * sizeof(buf_type), ir*256*sizeof(buf_type), 0);
+#define ACC_C(i_buf_) v_acc(acc, g_a.get(i_buf_), g_b.get(i_buf_))
+
+    // assume loop larget than 3
+    GLD_A(0); GLD_B(0); ir++;
+    GLD_A(1); GLD_B(1); ir++;
+
+#if DYNAMIC_BUF
+    while(ir < rows) {
+        GLD_A(2); GLD_B(2); ir++;
+        gld_fence(4);
+        ACC_C(0);
+        
+        if(ir >= rows) break;
+
+        GLD_A(0); GLD_B(0); ir++;
+        gld_fence(4);
+        ACC_C(1);
+
+        if(ir >= rows) break;
+
+        GLD_A(1); GLD_B(1); ir++;
+        gld_fence(4);
+        ACC_C(2);
+    }
+
+    if(mod == 0) {
+        gld_fence(2);
+        ACC_C(1);
+        gld_fence(0);
+        ACC_C(2);
+    }
+    else if(mod == 1) {
+        gld_fence(2);
+        ACC_C(2);
+        gld_fence(0);
+        ACC_C(0);
+    }
+    else {
+        gld_fence(2);
+        ACC_C(0);
+        gld_fence(0);
+        ACC_C(1);
+    }
+#else
+    if(mod == 0) {
+        GLD_A(2); GLD_B(2); ir++;
+        while(ir < rows) {
+            gld_fence(4);
+            ACC_C(0);
+            GLD_A(0); GLD_B(0); ir++;
+
+            gld_fence(4);
+            ACC_C(1);
+            GLD_A(1); GLD_B(1); ir++;
+
+            gld_fence(4);
+            ACC_C(2);
+            GLD_A(2); GLD_B(2); ir++;
+        }
+        gld_fence(4);
+        ACC_C(0);
+        gld_fence(2);
+        ACC_C(1);
+        gld_fence(0);
+        ACC_C(2);
+    }
+    else if(mod == 1) {
+        GLD_A(2); GLD_B(2); ir++;
+        gld_fence(4);
+        ACC_C(0);
+        GLD_A(0); GLD_B(0); ir++;
+        while(ir < rows) {
+            gld_fence(4);
+            ACC_C(1);
+            GLD_A(1); GLD_B(1); ir++;
+
+            gld_fence(4);
+            ACC_C(2);
+            GLD_A(2); GLD_B(2); ir++;
+
+            gld_fence(4);
+            ACC_C(0);
+            GLD_A(0); GLD_B(0); ir++;
+        }
+        gld_fence(4);
+        ACC_C(1);
+        gld_fence(2);
+        ACC_C(2);
+        gld_fence(0);
+        ACC_C(0);
+    }
+    else {
+        GLD_A(2); GLD_B(2); ir++;
+        gld_fence(4);
+        ACC_C(0);
+        GLD_A(0); GLD_B(0); ir++;
+
+        gld_fence(4);
+        ACC_C(1);
+        GLD_A(1); GLD_B(1); ir++;
+
+        while(ir < rows) {
+            gld_fence(4);
+            ACC_C(2);
+            GLD_A(2); GLD_B(2); ir++;
+
+            gld_fence(4);   
+            ACC_C(0);
+            GLD_A(0); GLD_B(0); ir++;
+
+            gld_fence(4);
+            ACC_C(1);
+            GLD_A(1); GLD_B(1); ir++;
+        }
+        gld_fence(4);
+        ACC_C(2);
+        gld_fence(2);
+        ACC_C(0);
+        gld_fence(0);
+        ACC_C(1);
+    }
+#endif
+    p_dst[col_offset] = acc;
+}
+#endif
 
 int main()
 {
