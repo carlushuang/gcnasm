df476536c2161d21ddf74c2d40bae455fd9cafbf
carlushuang <carlus.huang@amd.com>
2024-03-05 01:15:34 -0600

    update script

--- bandwidth_c/bench.sh
+++ bandwidth_c/bench.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 EXE=memcpy_kernel.exe
+export HIP_FORCE_DEV_KERNARG=1
 
+if [ $# -ge 1 ] ; then
+CASE=$1
+else
+CASE=0
+fi
+
+if [ "${CASE}" = "0" ] ; then
 ./${EXE} 20000
 ./${EXE} 400000
 ./${EXE} 9633792 # 3*64*224*224
@@ -9,4 +17,15 @@ EXE=memcpy_kernel.exe
 ./${EXE} 134217728 # 512M
 ./${EXE} 268435456 # 1G
 #./${EXE} 402653184 # 1.5G
-
+elif [ "${CASE}" = "1" ] ; then
+./${EXE} 78000
+./${EXE} 105000
+./${EXE} 155000
+./${EXE} 205000
+./${EXE} 270000
+./${EXE} 400000
+./${EXE} 530000
+./${EXE} 800000
+./${EXE} 1050000
+./${EXE} 1500000
+fi
