59a9838edd21291f0d254c772e85e10c8ac64f18
carlushuang <carlus.huang@amd.com>
2024-08-15 19:31:33 +0800

    refactor

--- memcpy_async/memcpy_async.cc
+++ memcpy_async/memcpy_async.cc
@@ -28,9 +28,21 @@ using fp32_t = float;
 
 using int32x4_t  = int32_t __attribute__((ext_vector_type(4)));
 
+using fp32x1_t = fp32_t __attribute__((ext_vector_type(1)));
+using fp32x2_t = fp32_t __attribute__((ext_vector_type(2)));
 using fp32x4_t = fp32_t __attribute__((ext_vector_type(4)));
 using fp32x16_t = fp32_t __attribute__((ext_vector_type(16)));
 using index_t  = int;
+
+template<int bytes>
+struct bytes_to_vector;
+
+template<> struct bytes_to_vector<16> { using type = fp32x4_t; };
+template<> struct bytes_to_vector<8> { using type = fp32x2_t; };
+template<> struct bytes_to_vector<4> { using type = fp32_t; };
+
+template<int bytes>
+using bytes_to_vector_t = typename bytes_to_vector<bytes>::type;
 
 
 
@@ -43,6 +55,11 @@ __host__ __device__ void for_(F func, std::index_sequence<Is...>)
     (func(num<Is>{}), ...);
 }
 
+template <typename Kernel, typename... Args>
+__global__ void kentry(Args... args)
+{
+    Kernel{}(args...);
+}
 
 #define BUFFER_LOAD_DWORD3 0x00020000
 struct buffer_resource {
@@ -163,63 +180,169 @@ __device__ __forceinline__ void nt_store(const T& value, T& ref) {
 }
 
 template<int BLOCK_SIZE = 256, int CHUNKS = 8>
-__global__ void memcpy_stream_async(void * src,
-    void * dst,
-    int bytes)
-{
-    int idx = (blockIdx.x * BLOCK_SIZE + threadIdx.x);
-    int wave_id = __builtin_amdgcn_readfirstlane(threadIdx.x / 64);
-    int total = bytes / 16; // dwordx4
-    // fp32x4_t * p_src = reinterpret_cast<fp32x4_t*>(src);
-    fp32x4_t * p_dst = reinterpret_cast<fp32x4_t*>(dst);
+struct memcpy_stream_async{
+    struct karg {
+        void * src;
+        void * dst;
+        int64_t bytes;
+    };
 
-    int32x4_t src_r = make_buffer_resource(src, bytes);
+    static constexpr int bytes_per_issue = 16;  // dwordx4
+    static constexpr int issues_per_block = BLOCK_SIZE * CHUNKS;
+    using vector_type = bytes_to_vector_t<bytes_per_issue>;
+    using harg = karg;
+    karg a;
+    dim3 grids;
 
-    __shared__ char smem[BLOCK_SIZE * sizeof(fp32x4_t)];
-
-    m0_set_with_memory(64 * sizeof(float) * wave_id);
-
-    for(auto c = 0; c < CHUNKS; c++) {
-        for_([&](auto i) {
-            async_buffer_load_dword_v(smem, src_r,
-                    c * gridDim.x * BLOCK_SIZE * sizeof(fp32x4_t) + blockIdx.x * BLOCK_SIZE * sizeof(fp32x4_t) + threadIdx.x * sizeof(float),
-                    0, i.value * sizeof(float) * BLOCK_SIZE);
-            // m0_inc_with_memory(BLOCK_SIZE * sizeof(float));
-            },
-            std::make_index_sequence<4>{}
-        );
-
-
-        buffer_fence(0);
-        __builtin_amdgcn_s_barrier();
-
-        auto d = reinterpret_cast<fp32x4_t*>(smem)[threadIdx.x];
-
-        auto current = idx + c * gridDim.x * BLOCK_SIZE;
-        if(current < total)
-            p_dst[current] = d;
-        __builtin_amdgcn_s_barrier();
+    __host__ memcpy_stream_async(harg harg_) {
+        a = harg_;
+        auto elems = a.bytes / bytes_per_issue;
+        grids = dim3((elems + issues_per_block - 1) / issues_per_block);
     }
-}
+
+    __host__ void operator()() const
+    {
+        kentry<kernel><<<grids, BLOCK_SIZE>>>(a);
+    }
+
+    struct kernel {
+        __device__ void operator()(harg args){
+            int64_t base_offset = blockIdx.x * BLOCK_SIZE * bytes_per_issue;
+            // int idx = (blockIdx.x * BLOCK_SIZE + threadIdx.x);
+            int wave_id = __builtin_amdgcn_readfirstlane(threadIdx.x / 64);
+            int32_t total = static_cast<int32_t>((args.bytes - base_offset) / bytes_per_issue);
+
+            vector_type * p_dst = reinterpret_cast<vector_type*>(reinterpret_cast<uint8_t*>(args.dst) + base_offset);
+            int32x4_t src_r = make_buffer_resource(reinterpret_cast<uint8_t*>(args.src) + base_offset, args.bytes - base_offset);
+
+            __shared__ char smem[BLOCK_SIZE * sizeof(vector_type)];
+
+            m0_set_with_memory(64 * sizeof(float) * wave_id);
+
+            constexpr int loops_per_vector = sizeof(vector_type)/sizeof(float);
+            for(auto c = 0; c < CHUNKS; c++) {
+                for_([&](auto i) {
+                    async_buffer_load_dword_v(smem, src_r,
+                            c * gridDim.x * BLOCK_SIZE * sizeof(vector_type) + threadIdx.x * sizeof(float),
+                            0, i.value * sizeof(float) * BLOCK_SIZE);
+                    // m0_inc_with_memory(BLOCK_SIZE * sizeof(float));
+                    },
+                    std::make_index_sequence<loops_per_vector>{}
+                );
+
+                buffer_fence(0);
+                __builtin_amdgcn_s_barrier();
+
+                auto d = reinterpret_cast<vector_type*>(smem)[threadIdx.x];
+
+                auto current = threadIdx.x + c * gridDim.x * BLOCK_SIZE;
+                if(current < total)
+                    p_dst[current] = d;
+                __builtin_amdgcn_s_barrier();
+            }
+        }
+    };
+};
 
 template<int BLOCK_SIZE = 256, int CHUNKS = 8>
-__global__ void memcpy_stream(void * src,
-    void * dst,
-    int bytes)
-{
-    int idx = (blockIdx.x * BLOCK_SIZE + threadIdx.x);
-    int total = bytes / 16; // dwordx4
-    fp32x4_t * p_src = reinterpret_cast<fp32x4_t*>(src);
-    fp32x4_t * p_dst = reinterpret_cast<fp32x4_t*>(dst);
-    for(auto c = 0; c < CHUNKS; c++) {
-        auto current = idx + c * gridDim.x * BLOCK_SIZE;
-        if(current < total) {
-            //auto d = nt_load(p_src[current]);
-            //nt_store(d, p_dst[current]);
-            p_dst[current] = p_src[current];
-        }
+struct memcpy_stream{
+    struct karg {
+        void * src;
+        void * dst;
+        int64_t bytes;
+    };
+
+    static constexpr int bytes_per_issue = 16;  // dwordx4
+    static constexpr int issues_per_block = BLOCK_SIZE * CHUNKS;
+    using vector_type = bytes_to_vector_t<bytes_per_issue>;
+    using harg = karg;
+    karg a;
+    dim3 grids;
+
+    __host__ memcpy_stream(harg harg_) {
+        a = harg_;
+        auto elems = a.bytes / bytes_per_issue;
+        grids = dim3((elems + issues_per_block - 1) / issues_per_block);
     }
-}
+
+    __host__ void operator()() const
+    {
+        kentry<kernel><<<grids, BLOCK_SIZE>>>(a);
+    }
+
+    struct kernel {
+        __device__ void operator()(harg args){
+            int64_t base_offset = blockIdx.x * BLOCK_SIZE * bytes_per_issue;
+            int total = static_cast<int32_t>((args.bytes - base_offset) / bytes_per_issue);
+            fp32x4_t * p_src = reinterpret_cast<fp32x4_t*>(reinterpret_cast<uint8_t*>(args.src) + base_offset);
+            fp32x4_t * p_dst = reinterpret_cast<fp32x4_t*>(reinterpret_cast<uint8_t*>(args.dst) + base_offset);
+            for(auto c = 0; c < CHUNKS; c++) {
+                auto current = threadIdx.x + c * gridDim.x * BLOCK_SIZE;
+                if(current < total) {
+                    //auto d = nt_load(p_src[current]);
+                    //nt_store(d, p_dst[current]);
+                    p_dst[current] = p_src[current];
+                }
+            }
+        }
+    };
+};
+
+template<int BLOCK_SIZE = 256, int CHUNKS = 8, int INNER = 1>
+struct memcpy_stream_swizzled{
+    struct karg {
+        void * src;
+        void * dst;
+        int64_t bytes;
+    };
+
+    static constexpr int bytes_per_issue = 16;  // dwordx4
+    static constexpr int issues_per_block = BLOCK_SIZE * CHUNKS * INNER;
+    using vector_type = bytes_to_vector_t<bytes_per_issue>;
+    using harg = karg;
+    karg a;
+    dim3 grids;
+
+    __host__ memcpy_stream_swizzled(harg harg_) {
+        a = harg_;
+        auto elems = a.bytes / bytes_per_issue;
+        grids = dim3((elems + issues_per_block - 1) / issues_per_block);
+    }
+
+    __host__ void operator()() const
+    {
+        kentry<kernel><<<grids, BLOCK_SIZE>>>(a);
+    }
+
+    struct kernel {
+        __device__ void operator()(harg args){
+            // simulate mfma 32x32x8(16)
+            constexpr int total_waves = BLOCK_SIZE / 64;
+            int wave_id = threadIdx.x / 64;
+            int lane_id = threadIdx.x % 64;
+            int lo_hi = lane_id / 32;
+            int sub_lane_id = lane_id % 32;
+
+            int64_t base_offset = blockIdx.x * BLOCK_SIZE * INNER * bytes_per_issue;
+            int idx = (lo_hi * 8 + wave_id * 16 + sub_lane_id * total_waves * 16) * INNER;
+
+            // int idx = (blockIdx.x * BLOCK_SIZE + threadIdx.x);
+            int total = (args.bytes - base_offset) / 2 ; /// 16; // dwordx4
+            ushort * p_src = reinterpret_cast<ushort*>(reinterpret_cast<uint8_t*>(args.src) + base_offset);
+            ushort * p_dst = reinterpret_cast<ushort*>(reinterpret_cast<uint8_t*>(args.dst) + base_offset);
+            for(auto c = 0; c < CHUNKS; c++) {
+                for(auto i = 0; i < INNER; i++) {
+                    auto current = idx + c * gridDim.x * BLOCK_SIZE * 8 * INNER + i * 8;
+                    if(current < total) {
+                        //auto d = nt_load(p_src[current]);
+                        //nt_store(d, p_dst[current]);
+                        *reinterpret_cast<fp32x4_t*>(p_dst + current) = *reinterpret_cast<fp32x4_t*>(p_src + current);
+                    }
+                }
+            }
+        }
+    };
+};
 
 #ifdef RAND_INT
 #define PER_PIXEL_CHECK
@@ -287,9 +410,9 @@ float bench_kernel(K k, int warmup, int loop)
 
 int main(int argc, char ** argv)
 {
-    int pixels = 64 * 1024 * 1024;
+    int64_t pixels = 64 * 1024 * 1024;
     if (argc >= 2)
-        pixels = atoi(argv[1]);
+        pixels = atoll(argv[1]);
 
     int *host_a, *host_b;
     void *dev_a, *dev_b;
@@ -322,7 +445,7 @@ int main(int argc, char ** argv)
 
     {
         auto k = [=](){
-            constexpr int block_size = 256;
+            constexpr int block_size = 1024;
             int grids = num_cu * 2;
             return [=](){
                 // printf("xxxxx\n");
@@ -340,11 +463,9 @@ int main(int argc, char ** argv)
     {
         auto k = [=](){
             constexpr int block_size = 256;
-            constexpr int chunks = 8;
-            int grids = ((pixels / 4) + (block_size*chunks) - 1)/ (block_size*chunks);
-            return [=](){
-                memcpy_stream<block_size, chunks><<<grids, block_size>>>(dev_a, dev_b, pixels * 4);
-            };
+            constexpr int chunks = 4;
+            auto k = memcpy_stream<block_size, chunks>{{dev_a, dev_b, pixels * 4}};
+            return k;
         }();
         HIP_CALL(hipMemset(dev_b, 0, pixels*sizeof(int)));
         auto ms = bench_kernel(k, WARMUP, LOOP);
@@ -357,11 +478,24 @@ int main(int argc, char ** argv)
     {
         auto k = [=](){
             constexpr int block_size = 256;
-            constexpr int chunks = 8;
-            int grids = ((pixels / 4) + (block_size*chunks) - 1)/ (block_size*chunks);
-            return [=](){
-                memcpy_stream_async<block_size, chunks><<<grids, block_size>>>(dev_a, dev_b, pixels * 4);
-            };
+            constexpr int chunks = 4;
+            auto k = memcpy_stream_async<block_size, chunks>{{dev_a, dev_b, pixels * 4}};
+            return k;
+        }();
+        HIP_CALL(hipMemset(dev_b, 0, pixels*sizeof(int)));
+        auto ms = bench_kernel(k, WARMUP, LOOP);
+        HIP_CALL(hipMemcpy(host_b, dev_b, pixels*sizeof(int), hipMemcpyDeviceToHost));
+        int res = valid_vector_integer(host_b, host_a, pixels);
+        (void) res;
+        printf("%.3f(GB/s) ", get_gbps(pixels * 2 * sizeof(int), ms)); fflush(stdout);
+    }
+
+    {
+        auto k = [=](){
+            constexpr int block_size = 256;
+            constexpr int chunks = 4;
+            auto k = memcpy_stream_swizzled<block_size, chunks>{{dev_a, dev_b, pixels * 4}};
+            return k;
         }();
         HIP_CALL(hipMemset(dev_b, 0, pixels*sizeof(int)));
         auto ms = bench_kernel(k, WARMUP, LOOP);
