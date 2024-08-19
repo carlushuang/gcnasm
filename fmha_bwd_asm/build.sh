5efd323ee96ea66a576f9e35444ab09f2e1831f0
nizhang <niels.zhang@amd.com>
2024-08-19 11:13:42 +0800

    default to atm f32

--- fmha_bwd_asm/build.sh
+++ fmha_bwd_asm/build.sh
@@ -1,5 +1,5 @@
 ARCH=gfx942
-/opt/rocm/llvm/bin/clang++ -x assembler -target amdgcn--amdhsa --offload-arch=$ARCH shaders/bwd_a16.s -o kernel.co
+/opt/rocm/llvm/bin/clang++ -x assembler -target amdgcn--amdhsa --offload-arch=$ARCH shaders/bwd_a32.s -o kernel.co
 /opt/rocm/bin/hipcc --offload-arch=$ARCH  bwd.cpp -o bwd.exe
 
 
