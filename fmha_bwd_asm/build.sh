9bcd2d1b86492fdf036414fdc2a14dcbb9c38da0
nizhang <niels.zhang@amd.com>
2024-08-26 09:31:42 +0000

    add missing file

--- fmha_bwd_asm/build.sh
+++ fmha_bwd_asm/build.sh
@@ -7,7 +7,9 @@ ARCH=gfx942
 ./bwd.exe b=1 h=16 s=16384 d=128 dump_result=0 init_pattern=0
 
 
-###run mask#####
+###run mask test#####
 ###Kernel: bwd_a32_m1k0.s , command_line: mask=1
 ###Kernel: bwd_a32_m1k1.s , command_line: mask=1 mask_kb=1
 
+####run in mi308####
+###use noCoex version###
