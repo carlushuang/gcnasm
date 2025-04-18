9ddedefa7461b3a207231855c45cb828cea0f0e3
carlushuang <carlus.huang@amd.com>
2025-04-18 17:12:04 +0800

    add 2 targets

--- matrix_core_asm/build.sh
+++ matrix_core_asm/build.sh
@@ -1,7 +1,7 @@
 ARCH=gfx90a
-/opt/rocm/llvm/bin/clang++ -x assembler -target amdgcn--amdhsa -mcpu=$ARCH  kernel.s -o kernel.co
-/opt/rocm/bin/hipcc -mcpu=$ARCH  bench.cpp -o bench.exe
-
+ARCH2=gfx942
+/opt/rocm/llvm/bin/clang++ -x assembler -target amdgcn--amdhsa -mcpu=$ARCH -mcpu=$ARCH2  kernel.s -o kernel.co
+/opt/rocm/bin/hipcc --offload-arch=$ARCH --offload-arch=$ARCH2  bench.cpp -o bench.exe
 
 # run
  ./bench.exe v_mfma_f32_16x16x1f32 16 16 1  1 64 1
