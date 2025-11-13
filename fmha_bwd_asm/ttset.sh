a2c1a8bd99526f339d6a2dfba8b4845bbbecd75e
Niels Zhang <nizhang@amd.com>
2025-11-13 10:35:26 +0800

    clean

--- fmha_bwd_asm/ttset.sh
+++
@@ -1,3 +0,0 @@
-cp libhsa-amd-aqlprofile64.so.1.0.0 /opt/rocm/lib/libhsa-amd-aqlprofile64.so.1.0.60100
-cp librocprofv2_att.so /opt/rocm/lib/hsa-amd-aqlprofile/
-sudo pip install numpy websockets matplotlib
