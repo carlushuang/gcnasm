cff883a8ec6023931d2aac7dd19e1f42afa744a4
kaiyang-1 <Kai.Yang1@amd.com>
2026-02-25 21:10:18 -0600

    8-wave gfx942 pipeline

--- opus_fmm/rebuild.sh
+++ opus_fmm/rebuild.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-SRC=flatmm.cc
+ARCH=${1:-gfx950}
+
+if [ "$ARCH" = "gfx950" ]; then
+    SRC=flatmm_gfx950.cc
+elif [ "$ARCH" = "gfx942" ]; then
+    SRC=flatmm_gfx942.cc
+else
+    echo "Unsupported architecture: $ARCH"
+    echo "Usage: ./rebuild.sh [gfx950|gfx942]"
+    exit 1
+fi
+
+echo "Building for architecture: $ARCH using source: $SRC"
+
 OUT=flatmm.exe
 TOP=`pwd`
 BUILD="$TOP/build/"
@@ -8,5 +21,4 @@ OPUS_INCLUDE_DIR=/path/to/aiter/csrc/include
 
 rm -rf $BUILD ; mkdir $BUILD ; cd $BUILD
 
-/opt/rocm/bin/hipcc $TOP/$SRC -I$OPUS_INCLUDE_DIR -fPIC -std=c++17 -fopenmp -O3 -Wall --offload-arch=gfx950 -save-temps -o $BUILD/$OUT
-
+/opt/rocm/bin/hipcc $TOP/$SRC -I$OPUS_INCLUDE_DIR -fPIC -std=c++17 -fopenmp -O3 -Wall --offload-arch=$ARCH -save-temps -o $BUILD/$OUT
