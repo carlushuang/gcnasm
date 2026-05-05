30d0df49fc7c3b1d10b5ec35c248443972dd17a9
KenSCLin <lshyhchy@amd.com>
2026-05-05 06:52:53 +0000

    Add support for hdim=256 in Flash Attention

--- opus_attn/gqa_gfx950_kernel_causal.cc
+++ opus_attn/gqa_gfx950_kernel_causal.cc
@@ -6,7 +6,9 @@
 #ifndef __HIP_DEVICE_COMPILE__
 template<typename Traits> __global__ void gqa_kernel(opus_gqa_kargs kargs) {}
 template __global__ void gqa_kernel<opus_gqa_traits<32, 64, 128, 8, true>>(opus_gqa_kargs);
+template __global__ void gqa_kernel<opus_gqa_traits<32, 64, 256, 4, true>>(opus_gqa_kargs);
 #else
 #include "gqa_gfx950_kernel_template.hpp"
 template __global__ void gqa_kernel<opus_gqa_traits<32, 64, 128, 8, true>>(opus_gqa_kargs);
+template __global__ void gqa_kernel<opus_gqa_traits<32, 64, 256, 4, true>>(opus_gqa_kargs);
 #endif
